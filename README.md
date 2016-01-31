bdd4a82b333fb69769df27ba4f98f68b7658c034
Cameron Knight <cjk8zb@mail.umkc.edu>
2016-01-31 17:35:33 -0600

    Commit version 2.0

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # UmpireBuddy
 
+## Version 2.0 (Lab #2)
+### IntroductionThe purpose of labs 1 and 2 is to give you practice with Android features you are likely to use this semester as you implement your project idea. Lab 1 gave you practice with Activities, UI components, event handlers and dialog boxes. Lab 2 adds: * Application icons  * Menus (action/tool bar and contextual menus) * Starting a new Activity * Persistent storage (saving state between sessions) * Screen Rotations (switching back and forth between portrait and landscape)<sup id="a1">[1](#f1)</sup> * Preferences (PreferenceActivity)### Part 1 – Application Icon
+Create a custom application launch icon for your application. For example:<img src="docs/doc-files/Lab2/Picture1.png" width="252" alt="Lab 2 - Picture 1">Hints:
+1.	Create an image using an image editing program such as Gimp. Make it square. Save it as a .png with a transparent background. Make sure file name is all lower case characters with no special symbols. [Save at different sizes if you want it to look good on a range of devices.]2.	Use Image Asset Studio to create images at different resolutions. (ref: http://developer.android.com/tools/help/image-asset-studio.html) (Manual way: Copy images at different resolutions to mipmap res subfolder"s. [May have to switch to Project View. If you have only one image, copy to any one of the subfolders.]3.	Reference the image in the manifest file under application/icon.
+For more information about drawable images in Android, see: http://developer.android.com/design/style/iconography.htmlThe app name on the home screen and the label on the main activity are specified in the AndroidManifest.xml file:<img src="docs/doc-files/Lab2/Picture2.png" width="327" alt="Lab 2 - Picture 2">The default name for both of these is the name of your app. You might want to change these to something more descriptive.### Part 2 - MenusAdd a menu to the main activity of the application. Include two options, one for resetting the count ("Reset") and another for displaying information about the application ("About"). Place the reset menu option icon on the action bar with text "Reset" if there is room. Make sure the About menu option always stays in the overflow area. For example:
+
+<img src="docs/doc-files/Lab2/Picture3.png" width="99" alt="Lab 2 - Picture 3"> and <img src="docs/doc-files/Lab2/Picture4.png" width="205" alt="Lab 2 - Picture 4">
+Note, you may have to rotate the screen to create enough room on the action/tool bar for the text "Reset" to display. On my emulator, I rotated the screen by turning off number lock and pressing the 9 key on the keypad.
+Selecting the Reset menu option should reset the count. Selecting the About menu option should display a new activity with information about the application. Be sure to include your name in the new activity. For example:<img src="docs/doc-files/Lab2/Picture5.png" width="194" alt="Lab 2 - Picture 5">
+I recommend using Android Studio's Image Asset program to create the image icon. You can create the icon from clipart:
+
+<img src="docs/doc-files/Lab2/Picture6.png" width="403" alt="Lab 2 - Picture 6">
+
+An alternative to creating your own drawable is to copy existing system drawables from the SDK. You can find these files at: `C:\Users\<user name>\AppData\Local\Android\sdk\platforms\android-23\data\res\drawable-hdpi`
+Another option is to reference one of the system icon images. Example: `icon="@android:drawable/ic_menu_add"`. However, these aren't consistent across devices.
+**[Extra Credit]** Provide a contextual menu using the contextual action mode on the background  of the main activity (or any UI component of your choosing) such that when the user long clicks a menu pops up that offers the ball and strike options. Here is an example with Count Much More:<img src="docs/doc-files/Lab2/Picture7.png" width="272" alt="Lab 2 - Picture 7">
+References:
+http://developer.android.com/guide/topics/ui/menus.html  http://developer.android.com/guide/topics/resources/menu-resource.html  http://developer.android.com/training/basics/firstapp/starting-activity.html### Part 3 – Persistent StorageAdd a field which counts total outs. The value should persist between sessions (even when user exits the app by pressing the back key). The only way to reset the value is by clearing the data for the app through settings.<img src="docs/doc-files/Lab2/Picture8.png" width="174" alt="Lab 2 - Picture 8">### Part 4 – Rotations [Extra Credit]Define a layout to be used in landscape mode that positions the buttons side-by-side when device is in landscape mode. The strike and ball count should remain unchanged when flipping between portrait and landscape.<img src="docs/doc-files/Lab2/Picture9.png" width="279" alt="Lab 2 - Picture 9">### Part 5 – Preferences [Extra credit]Add a menu option for application settings. Selecting settings should bring up a settings activity (I suggest using new style Preference Fragments ) that allows the user to select whether or not to announce "out" and "walk" audibly using Android's support for text-to-speech (tts). Preferences should of course persist between sessions. The setting should work. Great the user with an audible "Out" or "Walk" when appropriate.
 ## Version 1.0 (Lab #1)
 The purpose of this lab assignment is to help you get started with Android programming. It will introduce you to the syntax and semantics of Android/Java along with the mechanics of using Android Studio with the Android plugin.
 
@@ -7,13 +25,11 @@ The lab assignment is to create an Android application that can function as a ba
 
 For now, the application only needs to keep track of balls and strikes for a single batter. In a future assignment, I may have you extend it to keep track of outs and/or innings. (If you want to add extra features during assignment #1, have at it.)
 
-If the count reaches 3 strikes first, your application should display a dialog box announcing the batter is “Out!” If the count reaches 4 balls first, your application should display a dialog box announcing “Walk!” After either one, the count should be reset for the next batter.
+If the count reaches 3 strikes first, your application should display a dialog box announcing the batter is "Out!" If the count reaches 4 balls first, your application should display a dialog box announcing "Walk!" After either one, the count should be reset for the next batter.
 
 The final result should look something like:
 
-![Picture 1](docs/doc-files/Lab1Picture1.png)
-![Picture 2](docs/doc-files/Lab1Picture2.png)
-
+<img src="docs/doc-files/Lab1/Picture1.png" width="170" alt="Lab 1 - Picture 1"> <img src="docs/doc-files/Lab1/Picture2.png" width="172" alt="Lab 1 - Picture 2">
 
 ### Questions
 1. What, if anything, is created for you when you enter the following as a value in an .xml layout? `android:id="@+id/strike_button"`
@@ -21,3 +37,6 @@ The final result should look something like:
 
 2. What about when you enter: `android:text="@string/strike_button_label"`
   > This is a reference to a resource that is located in the string namespace.
+
+---
+<b id="f1">1</b> To rotate the emulator, turn off number lock then press <kbd>9</kbd> on the number pad (alternatively, use <kbd>control</kbd>+<kbd>F12</kbd>).  [↩](#a1)
--- app/src/main/java/edu/umkc/cs449/knight/cameron/umpirebuddy/PitchTrackerActivity.java
+++ app/src/main/java/edu/umkc/cs449/knight/cameron/umpirebuddy/PitchTrackerActivity.java
@@ -1,62 +1,90 @@
 package edu.umkc.cs449.knight.cameron.umpirebuddy;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.StringRes;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class PitchTrackerActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class PitchTrackerActivity extends AppCompatActivity implements ActionMode.Callback, TextToSpeech.OnInitListener, View.OnLongClickListener {
+    private static final String TAG = PitchTrackerActivity.class.getSimpleName();
 
     private static final String KEY_BALL_COUNT = "ball_count";
     private static final String KEY_STRIKE_COUNT = "strike_count";
+    private static final String KEY_OUT_COUNT = "out_count";
 
     private static final int MAX_BALLS = 4;
     private static final int MAX_STRIKES = 3;
 
     private int mBallCount;
+    private int mOutCount;
     private int mStrikeCount;
 
+    private ActionMode mActionMode;
     private Button mBallButton;
     private Button mStrikeButton;
+    private TextToSpeech mTextToSpeech;
     private TextView mBallsTextView;
     private TextView mStrikesTextView;
+    private TextView mOutsTextView;
 
+    /**
+     * extends AppCompatActivity
+     **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pitch_tracker);
+        findViewById(R.id.relative_layout).setOnLongClickListener(this);
 
         if (savedInstanceState != null) {
             mBallCount = savedInstanceState.getInt(KEY_BALL_COUNT);
             mStrikeCount = savedInstanceState.getInt(KEY_STRIKE_COUNT);
         }
+        mOutCount = PreferenceManager.getDefaultSharedPreferences(this).getInt(KEY_OUT_COUNT, 0);
 
         mBallButton = (Button) findViewById(R.id.ball_button);
         mStrikeButton = (Button) findViewById(R.id.strike_button);
         mBallsTextView = (TextView) findViewById(R.id.balls_text_view);
         mStrikesTextView = (TextView) findViewById(R.id.strikes_text_view);
+        mOutsTextView = (TextView) findViewById(R.id.outs_text_view);
 
         mBallButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mBallCount++;
-                updateUI();
+                ball();
             }
         });
 
         mStrikeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mStrikeCount++;
-                updateUI();
+                strike();
             }
         });
 
         updateUI();
+
+        mTextToSpeech = new TextToSpeech(this, this);
     }
 
     @Override
@@ -66,10 +94,179 @@ public class PitchTrackerActivity extends AppCompatActivity {
         outState.putInt(KEY_BALL_COUNT, mBallCount);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pitch_tracker, menu);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            applyMenuColor(actionBar.getThemedContext(), menu);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_about:
+                Intent about = new Intent(this, AboutActivity.class);
+                startActivity(about);
+                return true;
+            case R.id.menu_item_reset:
+                reset();
+                return true;
+            case R.id.menu_item_settings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * helper methods
+     **/
+
+    private void applyMenuColor(Context context, Menu menu) {
+        if (context == null || menu == null) {
+            return;
+        }
+
+        final TypedValue typedValue = new TypedValue();
+        if (!context.getTheme().resolveAttribute(android.R.attr.textColorPrimary, typedValue, true)) {
+            return;
+        }
+        Integer textColorId = typedValue.resourceId;
+
+        for (int i = 0; i < menu.size(); ++i) {
+            MenuItem menuItem = menu.getItem(i);
+            Drawable drawable = menuItem.getIcon();
+            if (drawable == null) {
+                continue;
+            }
+
+            drawable = DrawableCompat.wrap(drawable);
+            drawable.mutate();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                DrawableCompat.setTint(drawable, getResources().getColor(textColorId, null));
+            } else {
+                //noinspection deprecation
+                DrawableCompat.setTint(drawable, getResources().getColor(textColorId));
+            }
+            menuItem.setIcon(drawable);
+        }
+    }
+
+    /**
+     * interface ActionMode.Callback
+     **/
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        MenuInflater inflater = mode.getMenuInflater();
+        inflater.inflate(R.menu.contextual, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contextual_ball:
+                ball();
+                mode.finish();
+                return true;
+            case R.id.contextual_strike:
+                strike();
+                mode.finish();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        mActionMode = null;
+    }
+
+    /**
+     * interface TextToSpeech.OnInitListener
+     **/
+
+    @Override
+    public void onInit(int status) {
+        if (status != TextToSpeech.SUCCESS) {
+            Log.e(TAG, "Could not initialize TextToSpeech.");
+            return;
+        }
+
+        int result = mTextToSpeech.setLanguage(Locale.US);
+        if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            Log.e(TAG, "Language is not available.");
+        }
+    }
+
+    /**
+     * interface View.OnLongClickListener
+     **/
+
+    @Override
+    public boolean onLongClick(View v) {
+
+        if (mActionMode != null) {
+            return false;
+        }
+
+        mActionMode = this.startActionMode(this);
+        v.setSelected(true);
+        return true;
+    }
+
+    private void ball() {
+        mBallCount++;
+        updateUI();
+    }
+
+    private void strike() {
+        mStrikeCount++;
+        updateUI();
+    }
+
+    private void reset() {
+        mBallCount = 0;
+        mStrikeCount = 0;
+        mStrikeButton.setEnabled(true);
+        mBallButton.setEnabled(true);
+        updateUI();
+    }
+
+    private void updateUI() {
+        if (mBallCount >= MAX_BALLS) {
+            showAlert(R.string.max_balls_message, R.string.max_balls_confirm);
+        } else if (mStrikeCount >= MAX_STRIKES) {
+            mOutCount++;
+            PreferenceManager.getDefaultSharedPreferences(this)
+                    .edit()
+                    .putInt(KEY_OUT_COUNT, mOutCount)
+                    .apply();
+            showAlert(R.string.max_strikes_message, R.string.max_strikes_confirm);
+        }
+
+        mStrikesTextView.setText(String.format(getString(R.string.count_format), mStrikeCount));
+        mBallsTextView.setText(String.format(getString(R.string.count_format), mBallCount));
+        mOutsTextView.setText(String.format(getString(R.string.count_format), mOutCount));
+    }
+
     private void showAlert(@StringRes int messageId, @StringRes int textId) {
         mStrikeButton.setEnabled(false);
         mBallButton.setEnabled(false);
 
+        say(getString(messageId) + getString(textId));
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(messageId)
                 .setTitle(R.string.alert_title)
@@ -77,24 +274,23 @@ public class PitchTrackerActivity extends AppCompatActivity {
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
-                        mBallCount = 0;
-                        mStrikeCount = 0;
-                        mStrikeButton.setEnabled(true);
-                        mBallButton.setEnabled(true);
-                        updateUI();
+                        reset();
                     }
                 })
                 .show();
     }
 
-    private void updateUI() {
-        if (mBallCount >= MAX_BALLS) {
-            showAlert(R.string.max_balls_message, R.string.max_balls_confirm);
-        } else if (mStrikeCount >= MAX_STRIKES) {
-            showAlert(R.string.max_strikes_message, R.string.max_strikes_confirm);
+    private void say(String message) {
+        boolean announce = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean(this.getString(R.string.preference_announce_key), false);
+        if (announce) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mTextToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
+            } else {
+                //noinspection deprecation
+                mTextToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+            }
         }
-
-        mStrikesTextView.setText(String.format(getString(R.string.count_format), mStrikeCount));
-        mBallsTextView.setText(String.format(getString(R.string.count_format), mBallCount));
     }
+
 }
